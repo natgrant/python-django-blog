ab63bdffe17d15cb8cdef804ac052c2e6b6c5106
Nat Grant <email.natgrant@gmail.com>
2019-03-26 11:59:16 +1300

    set up urls for crud actions

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def post_create(request):
+    return HttpResponse("<h1>Hai</h1>")
+
+
+def post_detail(request):
+    return HttpResponse("<h1>Hai</h1>")
+
+
+def post_list(request):
+    return HttpResponse("<h1>Hai</h1>")
+
+
+def post_update(request):
+    return HttpResponse("<h1>Hai</h1>")
+
+
+def post_delete(request):
+    return HttpResponse("<h1>Hai</h1>")
