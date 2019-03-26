048ce365464a60dec274b0aa3cf33965b1d4d515
Nat Grant <email.natgrant@gmail.com>
2019-03-26 17:47:28 +1300

    add namespace for url specificity

--- posts/urls.py
+++ posts/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from . import views
+from .views import (
+    post_list,
+    post_create,
+    post_detail,
+    post_delete,
+    post_update,
+)
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    url(r'^create/$', views.post_create),
-    url(r'^(?P<id>\d+)/$', views.post_detail),
-    url(r'^update/$', views.post_update),
-    url(r'^delete/$', views.post_delete),
+    url(r'^$', post_list),
+    url(r'^create/$', post_create),
+    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
 ]
