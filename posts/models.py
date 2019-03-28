cfd399922e7823d725b4a45d42d3a3c72a8d3289
Nat Grant <email.natgrant@gmail.com>
2019-03-28 13:27:30 +1300

    implement slug

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import pre_save
+
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -12,6 +15,7 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location, null=True, blank=True,
                               height_field="height_field", width_field="width_field")
     height_field = models.IntegerField(default=0)
@@ -28,3 +32,14 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-timestamp", "-updated"]
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    slug = slugify(instance.title)
+    exists = Post.objects.filter(slug=slug).exists()
+    if exists:
+        slug = "%s-%s" % (slug, instance.id)
+    instance.slug = slug
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
